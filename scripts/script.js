7a99b6756a6124e25dd6c86b5856578122adfad2
Koen_Haagsma <koenhaagsma123@hotmail.nl>
2020-11-24 11:38:56 +0100

    Small changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
 // https://www.w3schools.com/howto/howto_js_sticky_header.asp
-// W3schools gebruikt hoe ik de header van sticky naar normaal kan laten gaan
-// Dit omdat ik niet precies wist hoe het moest, code is wel duidelijk beetje aangepast om hetn aar m'n eigen stijl te doen
 
 window.onscroll = function () {
     fixedHeader();
